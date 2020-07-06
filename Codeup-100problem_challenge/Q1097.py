1569cf0f5d150728c1c1dc69c4c8d91ae38b1f04
Roy Joo <52684315+djoo1028@users.noreply.github.com>
2020-07-05 18:19:01 -0700

    Add files via upload
    Similar question as Q1096.

---
+++ Codeup-100problem_challenge/Q1097.py
@@ -0,0 +1,61 @@
+'''
+print matrix and change number 1 to 0 or 0 to 1 where
+
+it is selected
+
+example
+
+first input
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+second input
+2
+third input
+10 10
+12 12
+
+'''
+
+arr2d = []
+
+for i in range(19):
+    arr2d.append(input().split())
+
+x = int(input())
+
+for i in range(x):
+    a,b = input().split()
+    a = int(a) - 1
+    b = int(b) - 1
+
+    for j in range(19):
+        if(arr2d[j][b] == '0'):
+            arr2d[j][b] = '1'
+        else:
+            arr2d[j][b] = '0'
+        if(arr2d[a][j] == '0'):
+            arr2d[a][j] = '1'
+        else:
+            arr2d[a][j] = '0'
+
+for i in range(19):
+    for j in range(19):
+        print(arr2d[i][j], end = ' ')
+    print('')
